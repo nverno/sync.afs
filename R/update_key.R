26308a004a0a99fd838294ff94a857e2c2cec7a0
Noah Peart <noah.v.peart@gmail.com>
2015-12-22 02:31:16 -0500

    Batch Update

--- R/update_key.R
+++ R/update_key.R
@@ -5,7 +5,7 @@
 update_key <- function(path=get_afs(), tracker="file_tracker.txt", data_key=data_key) {
   tracker <- file.path(path, tracker)
   dat <- process_tracker(tracker)
-    
+  
   ## New files
   inds <- which(!(dat$files %in% data_key[['filename']]))
 
@@ -13,17 +13,18 @@ update_key <- function(path=get_afs(), tracker="file_tracker.txt", data_key=data
   if (length(dat$renamed)) {
     old_names <- unlist(lapply(dat$renamed, `[[`, 1), use.names=FALSE)
     new_names <- unlist(lapply(dat$renamed, `[[`, 2), use.names=FALSE)
-    data_key[sapply]
-  
-  ## Update key with renamed files/new files
-  if (length(dat$renamed) | length(inds)) {
-    new_files <- c(dat$files[inds], unlist(lapply(dat$renamed, `[[`, 2), use.names = FALSE))
-    finfo <- file_info(path, files=new_files)
-    if (nrow(finfo)) {
-      if (length(dat$renamed)) {
-        
-        rbind(copy(data_key))[finfo, rname := i.rname, on = 'filename'][]
-        rename_inds <- data_key[match(oldnames, filename, 0L), ]
+    data_key[filename]
+    
+    ## Update key with renamed files/new files
+    if (length(dat$renamed) | length(inds)) {
+      new_files <- c(dat$files[inds], unlist(lapply(dat$renamed, `[[`, 2), use.names = FALSE))
+      finfo <- file_info(path, files=new_files)
+      if (nrow(finfo)) {
+        if (length(dat$renamed)) {
+          
+          rbind(copy(data_key))[finfo, rname := i.rname, on = 'filename'][]
+          rename_inds <- data_key[match(oldnames, filename, 0L), ]
+        }
       }
     }
   }
