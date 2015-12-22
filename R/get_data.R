d8ef567e2c35c981c2032d13512404b74267e7fb
Noah Peart <noah.v.peart@gmail.com>
2015-12-22 17:49:02 -0500

    get_data working

--- R/get_data.R
+++ R/get_data.R
@@ -3,7 +3,7 @@ NULL
 
 ##' Read master data associated with R data file.
 ##' @title Read master data file corresponding to a R data filename
-##' @import haven
+##' @importFrom haven read_sas
 ##' @import data.table
 ##' @param dname data name in R
 ##' @param data_key Key matching R data to master files.
@@ -14,10 +14,9 @@ NULL
 ##' }
 ##' @return corresponding master file (prior to cleaning)
 ##' @export
-get_data <- function(dname, data_key=data_key, ...) {
-  if (!exists("data_key"))
+get_data <- function(dname, dkey=data_key, ...) {
+  if (!exists("dkey"))
     stop('data_key isn\'t made, use the create_data_key_template() function.')
-  dkey <- data.table::copy(data_key)
   if (!(dname %in% dkey[['rname']]))
     stop('That data is not named in the key.')
   rname <- filetype <- afs_path <- NULL
@@ -25,7 +24,7 @@ get_data <- function(dname, data_key=data_key, ...) {
   ## choose how to read
   ftype = dkey[rname == dname, filetype]
   read <- switch(ftype,
-                 'sas7bdat' = read_sas,
+                 'sas7bdat' = haven::read_sas,
                  'csv' = read.csv,
                  'txt' = read.table,
                  read.table)
