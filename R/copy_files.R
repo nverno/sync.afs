00c0f81765d1fb01869f1136754d7143de317a29
Noah Peart <noah.v.peart@gmail.com>
2016-01-07 12:52:16 -0500

    minor fixes to file copy

--- R/copy_files.R
+++ R/copy_files.R
@@ -2,15 +2,18 @@
 ##' @title Copy files from AFS 
 ##' @param ext File extensions (character vector)
 ##' @param outdir Name of directory to copy files to
+##' @param dkey Data key
+##' @param path Path to root AFS node.
 ##' @param ... Passed to \code{file.copy}
 ##' @return TRUE if all files copied successfully.
 ##' @export
-copy_files <- function(ext, outdir='temp', ...) {
+copy_files <- function(ext, outdir='temp', dkey=sync.afs::data_key, 
+                       path=get_afs(), ...) {
     check_afs()
-    filetype <- afs_path <- data_key <- NULL
-    filepaths <- data_key[filetype %in% ext, afs_path]
+    filetype <- afs_path <- NULL
+    filepaths <- dkey[filetype %in% ext, afs_path]
     dir.create(outdir)
-    res <- file.copy(from = file.path(get_afs(), filepaths),
+    res <- file.copy(from = file.path(path, filepaths),
                      to = outdir, ...)
     if (!(any(res))) {
         warning(sprintf("Failed to copy %s", paste(basename(filepaths), collapse=",")))
